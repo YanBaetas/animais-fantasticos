14bab9c6106123f38c165991108de07c85a1f8b3
Yan Oliveira <yan_baetas@hotmail.com>
2021-01-15 11:43:11 -0300

    fetch-animais refatorado

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 // import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -32,5 +32,5 @@ initDropdownMenu();
 initMenuMobile();
 // initAnimaNumeros();
 initFuncionamento();
-initFetchAnimais();
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 initFetchBitcoin();
